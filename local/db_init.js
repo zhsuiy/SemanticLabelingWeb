b12d56a60bf61dfc253f08c663310adf304c1889
zhxilin <zhxilin@live.com>
2017-12-06 01:38:23 +0800

    Update output logic

--- local/db_init.js
+++ local/db_init.js
@@ -5,13 +5,26 @@ var db = require('./database'),
 var GROUPS = 'groups',
     GROUP_PREFIX = 'group-';
 
-client.on('ready', function () {
-    init();
-});
-
-function init() {
-    for (var i = 0; i < GROUP_NUM; i++) {
-        client.zadd(GROUPS, 0, GROUP_PREFIX + i);
-    }
-    console.log('DB init groups label count succeed.');
+function initGroups() {
+    client.on('ready', function () {
+        for (var i = 0; i < GROUP_NUM; i++) {
+            client.zadd(GROUPS, 0, GROUP_PREFIX + i);
+        }
+        console.log('DB init groups label count succeed.');
+    });
 }
+
+function clearData() {
+    client.on('ready', function () {
+        client.flushdb(function (err, reply) {
+            console.log('DB flush.');
+        });
+    });
+}
+
+module.exports = {
+    initGroups,
+    clearData
+}
+
+require('make-runnable');
